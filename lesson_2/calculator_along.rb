c777ae7c924935a9cb4288eaf133be32200b1ec8
John Caudle <jmcaudle.ls@gmail.com>
2023-11-27 23:44:40 -0600

    Internationalize the calculator

--- lesson_2/calculator_along.rb
+++ lesson_2/calculator_along.rb
@@ -3,60 +3,88 @@
 # perform the operation on the two numbers
 # output the result
 
-def prompt(message)
+DEFAULT_LANG = 'en'
+$language = DEFAULT_LANG
+
+require 'yaml'
+MESSAGES = YAML.load_file 'calculator_messages.yml'
+# puts MESSAGES.inspect
+
+def messages(message, lang = 'en')
+  MESSAGES[lang][message]
+end
+
+def prompt(key, interpolation = nil)
+  message = messages(key, $language)
+  message = format(message, interpolation) if interpolation
   puts "=> #{message}"
 end
 
 def valid_number?(number)
-  number.to_i != 0
+  number.to_i.to_s == number || number.to_f.to_s == number
 end
+
+# def int?(string)
+#   string.to_i.to_s == string
+# end
 
 def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
+  # op_string = case op
+  #             when '1'
+  #               'Adding'
+  #             when '2'
+  #               'Subtracting'
+  #             when '3'
+  #               'Multiplying'
+  #             when '4'
+  #               'Dividing'
+  #             end
+  # n = 1 + 2
+  # op_string  
+  MESSAGES[$language]['op_verb'][op.to_i - 1]
 end
 
-prompt "Welcome to Calculator! Enter your name:"
+prompt 'change_language'
+lang = gets.chomp.downcase
+if lang.start_with? 'es'
+  $language = 'es'
+elsif lang. start_with? 'fr'
+  $language = 'fr'
+end
+
+prompt('welcome')
 
 name = ''
 loop do
   name = gets.chomp
   if name.empty?
-    prompt "Please input a valid name."
+    prompt 'valid_name'
   else
     break
   end
 end
-prompt "Hi, #{name}!"
+prompt('named_greet', {:name => name})
 
 loop do
   number1 = ''
   loop do
-    prompt "What's the first number?"
+    prompt 'first_num'
     number1 = gets.chomp
     if valid_number? number1
       break
     else
-      prompt "That doesn't look like a valid number"
+      prompt 'valid_num'
     end
   end
 
   number2 = ''
   loop do
-    prompt "What's the second number?"
+    prompt 'second_num'
     number2 = gets.chomp
     if valid_number? number2
       break
     else
-      prompt "That doesn't look like a valid number"
+      prompt 'valid_num'
     end
   end
 
@@ -67,7 +95,7 @@ What Operation would you like to perform?
     3) multiply
     4) divide
     MSG
-  prompt operator_prompt
+  prompt 'op_prompt'
 
   operator = ''
   loop do
@@ -75,11 +103,11 @@ What Operation would you like to perform?
     if ('1'..'4').include? operator
       break
     else
-      prompt "Must choose 1, 2, 3, or 4"
+      prompt 're_op'
     end
   end
 
-  prompt "#{operation_to_message(operator)} the two numbers..."
+  prompt 'confirmation', { op_word: operation_to_message(operator)}
 
   result =  case operator
             when '1'
@@ -91,11 +119,11 @@ What Operation would you like to perform?
             when '4'
               number1.to_f / number2.to_f
             end
-  prompt "The result is #{result}"
+  prompt 'answer', result: result
 
-  prompt "Do you want to perform another calculation? (Y to calculate again)"
+  prompt 're_calc'
   answer = gets.chomp
-  break unless answer.downcase[0] == 'y'
+  break unless answer.downcase[0] == MESSAGES[$language]['yes_letter']
 end
 
-prompt "Thank you for using The Calculator, Good Bye."
+prompt 'goodbye'
