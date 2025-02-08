ecf3843c47409716b013fd387b0dc25b6cc0bfd1
JMCaudle <jmcaudle@gmail.com>
2025-02-07 18:30:27 -0600

    testing

--- small_problems_easy_2/teddy/teddy.rb
+++ small_problems_easy_2/teddy/teddy.rb
@@ -2,3 +2,5 @@ puts "Please input the name of the entity whose age you wish to know:"
 name = gets.chomp
 name = "Teddy" if name == ''
 puts "#{name} is #{rand(20..200)} years old!"
+
+# a commitable change
